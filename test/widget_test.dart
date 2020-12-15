55c1f62044efc1236dc7c0a3196f3b94a0001b95
Trần Ngô Triệu Tuấn <Tran Ngo Trieu Tuan>
2020-12-15 10:26:15 +0700

    Fix test again

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tigerdate/app/my_app.dart';
+import 'package:tigerdate/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
